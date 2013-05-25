427997abf189a0589701dfaf21f2fe1d9ffe4ae2
lichnow <lichnow@gmail.com>
2013-05-25 14:36:40 +0800

    test

---
+++ src/Site/LessonBundle/DataFixtures/MongoDB/LoadVideoData.php
@@ -0,0 +1,91 @@
+<?php 
+namespace Site\LessonBundle\DataFixtures\MongoDB;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Site\LessonBundle\Document\Video;
+
+class LoadVideoData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $videos = array(
+            array(
+                'title' => 'Symfony2安装及环境搭建',
+                'order' => 0,
+                'playtimes' => 0,
+                'downpath' => 'uploads/videos/home/homeimg.mp4',
+                'imgpath' => 'uploads/videos/home/homeimg.jpg',
+                'public' => true,
+                'ctime' => new \DateTime()
+            ),
+            array(
+                'title' => 'Rails3安装及环境搭建',
+                'order' => 0,
+                'playtimes' => 0,
+                'downpath' => 'uploads/videos/home/homeimg.mp4',
+                'imgpath' => 'uploads/videos/home/homeimg.jpg',
+                'public' => true,
+                'ctime' => new \DateTime()
+            ),
+            array(
+                'title' => 'Silex安装及环境搭建',
+                'order' => 0,
+                'playtimes' => 0,
+                'downpath' => 'uploads/videos/home/homeimg.mp4',
+                'imgpath' => 'uploads/videos/home/homeimg.jpg',
+                'public' => true,
+                'ctime' => new \DateTime()
+            ),
+            array(
+                'title' => 'Bootstrap使用初步',
+                'order' => 0,
+                'playtimes' => 0,
+                'downpath' => 'uploads/videos/home/homeimg.mp4',
+                'imgpath' => 'uploads/videos/home/homeimg.jpg',
+                'public' => true,
+                'ctime' => new \DateTime()
+            ),
+            array(
+                'title' => 'Nginx+PHP+Rails服务器搭建',
+                'order' => 0,
+                'playtimes' => 0,
+                'downpath' => 'uploads/videos/home/homeimg.mp4',
+                'imgpath' => 'uploads/videos/home/homeimg.jpg',
+                'public' => true,
+                'ctime' => new \DateTime()
+            )
+        );
+        $this->insertData($manager,$videos);
+    }
+
+    protected function insertData($manager,$videos)
+    {
+        foreach ($videos as $key => $value) {
+            $video = new Video();
+            $video->setTitle($value['title']);
+            $video->setOrder($value['order']);
+            $video->setPlaytimes($value['playtimes']);
+            $video->setDownpath($value['downpath']);
+            $video->setImgpath($value['imgpath']);
+            $video->setCtime($value['ctime']);
+            $video->setPublic($value['public']);
+            $video->setLesson($this->getReference('lesson'));
+            $manager->persist($video);
+            $manager->flush();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 4;
+    }
+}
+ ?>
